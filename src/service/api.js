6aa2abae4b6316cd12a428cfb871513e53a76f6f
GABRIEL MACHADO DE JESUS <gabriel_m_de-jesus@estudante.sesisenai.org.br>
2023-11-30 20:06:09 -0300

    fix: ajuste do token para duas semanas

--- src/service/api.js
+++ src/service/api.js
@@ -4,7 +4,7 @@ export const apiLaudelino = axios.create({
   baseURL: "https://cardapios-mktplace-api-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJwYXBlbCI6IkxPSklTVEEiLCJzdWIiOiJ1c3VhcmlvNS5sb2ppc3RhIiwiaWF0IjoxNzAxMzY1ODA3LCJleHAiOjE3MDEzNjc2MDd9.ST7HA9D5H3bhxm1w9q2HFZlTdwuJK1CbWLeBGpYwRtM`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJwYXBlbCI6IkxPSklTVEEiLCJzdWIiOiJ1c3VhcmlvNS5sb2ppc3RhIiwiaWF0IjoxNzAxMzg1MzkxLCJleHAiOjE3MDI1OTQ5OTF9.8BtJSR91i-Sich0HRayou-ABfZ4qT6_FOwPHuiC2arU`,
   },
 });
 
