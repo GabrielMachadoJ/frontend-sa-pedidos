a655daa8c31bbcbdd6eae9af67a8e2187f27d93e
Gabriel <gabriel.machado@frengee.com>
2023-12-02 23:05:17 -0300

    refactor: passando o token para uma variavel de ambiente

--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const token = process.env.REACT_APP_TOKEN_LAUDELINO;
+
 export const apiLaudelino = axios.create({
   baseURL: "https://cardapios-mktplace-api-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJwYXBlbCI6IkxPSklTVEEiLCJzdWIiOiJ1c3VhcmlvNS5sb2ppc3RhIiwiaWF0IjoxNzAxMzg1MzkxLCJleHAiOjE3MDI1OTQ5OTF9.8BtJSR91i-Sich0HRayou-ABfZ4qT6_FOwPHuiC2arU`,
+    Authorization: `Bearer ${token}`,
   },
 });
 
