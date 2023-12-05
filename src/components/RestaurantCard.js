2a22ec202277523bb54f6baf751757ddee65b11f
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-12-05 10:06:13 -0300

    Removido informações estáticas do componente de restaurante.

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -77,41 +77,12 @@ export default function RestaurantCard(params) {
                         fontSize: "1rem",
                       }}
                     >
-                      <span style={{ color: "#ff9558", fontWeight: "bold" }}>
-                        ⭐4.8
-                      </span>{" "}
-                      | {rest.categoria.nome} | 1.5 km
+                      <span style={{
+                         color: "#ff9558",
+                         fontWeight: "bold" }}>
+                          </span>{" "} {rest.categoria.nome}
+                          {" "}
                     </span>
-                    <span style={{ color: "#a39e9e", marginBottom: ".5rem" }}>
-                      25-35min |{" "}
-                      <span style={{ color: "#13c51b" }}>Grátis</span>
-                    </span>
-                    <div
-                      style={{
-                        backgroundColor: "#c4e1f741",
-                        display: "flex",
-                        alignItems: "center",
-                        padding: ".2rem .4rem",
-                        borderRadius: ".3rem",
-                      }}
-                    >
-                      <Ticket
-                        color="#4e6aac"
-                        size={20}
-                        style={{
-                          marginRight: ".3rem",
-                        }}
-                      />
-                      <span
-                        style={{
-                          color: "#4e6aac",
-                          fontSize: ".9rem",
-                          fontWeight: 500,
-                        }}
-                      >
-                        Cupom de R$ 5 disponível
-                      </span>
-                    </div>
                   </div>
                 </div>
               </Link>
